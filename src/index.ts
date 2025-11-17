5884da7caaf65ff4d7eb9b3d4db53e52b4696f03
TheSaurojit <saurojitkarmakar947@gmail.com>
2025-11-17 10:38:28 +0530

    env config

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import { env } from "./config/env";
 
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   console.log(process.env.PORT);
-  
-return  res.send("Hello from Express + TS!!!");
+
+  return res.send("Hello from Express + TS!!!");
 });
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(env.PORT, () => {
+  console.log("Server running on port " + env.PORT);
 });
